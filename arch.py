f44c919a9bbd2b1b4704d3b8a1b58d74872d307a
hazelash <32414861+hazelash@users.noreply.github.com>
2020-08-14 13:14:31 +1000

    Create arch.py

---
+++ arch.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import pefile
+import re
+
+def list_files(path):
+	files = []
+	for name in os.listdir(path):
+		if os.path.isfile(os.path.join(path, name)):
+			files.append(name)
+	return files
+
+all_files = list_files('%s' % (sys.argv[1]))
+
+for file in all_files:
+	newpath = '%s\\%s'% (sys.argv[1], file)
+	
+	logbuf = ''
+	logbuf += ("%s " % file)
+	
+	f = open(newpath, 'rb')
+	buf = f.read(0x10)
+	
+	num_sections_delphi = 0
+	export_name_count = 0;
+	random_export_name_count = 0
+	
+	if ord(buf[0]) != 0x4d or ord(buf[1]) != 0x5a:
+		logbuf += ("not pe")
+		print logbuf
+		f.close()	
+		continue 
+
+	pe = pefile.PE(newpath, fast_load = True)
+	
+	if ( pe.FILE_HEADER.Machine ):
+		print "%s\t%s\t| %s" % (file, pe.FILE_HEADER.Machine,pe.OPTIONAL_HEADER.Subsystem)
+    		
+	f.close()
+	
+		
